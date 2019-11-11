f3483f70fdacef359fcc7d4c6dc3698d78e189ed
Greg Wiley <aztec.rex@jammm.com>
2019-11-10 16:14:46 -0800

    user pool

--- lib/cognito-spike-stack.ts
+++ lib/cognito-spike-stack.ts
@@ -1,9 +1,58 @@
 import cdk = require('@aws-cdk/core');
+import cognito = require('@aws-cdk/aws-cognito');
+import iam = require('@aws-cdk/aws-iam');
 
 export class CognitoSpikeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+
+    const snsRole = new iam.Role(this, "UsersSNSRole", {
+      assumedBy: new iam.ServicePrincipal("cognito-idp.amazonaws.com"),
+    });
+    const snsPublishPolicy = new iam.ManagedPolicy(this, "SNSPublish", {
+      statements: [new iam.PolicyStatement(
+        {
+          actions: ["sns:publish"],
+          resources: ['*'],
+          }
+      )],
+    });
+    snsRole.addManagedPolicy(snsPublishPolicy);
+
+    const pool = new cognito.CfnUserPool(this, "users", {
+      autoVerifiedAttributes: ["email"],
+      usernameAttributes: ["email"],
+      policies: {
+        passwordPolicy: {
+          minimumLength: 8,
+          requireLowercase: false,
+          requireUppercase: false,
+          requireNumbers: false,
+          requireSymbols: false,
+        },
+
+      },
+      adminCreateUserConfig: {
+        allowAdminCreateUserOnly: false,
+      },
+      mfaConfiguration: "ON",
+      enabledMfas: ["SMS_MFA"],
+      smsConfiguration: {
+        externalId: "foobarId",
+        snsCallerArn: snsRole.roleArn,
+      },
+      deviceConfiguration: {
+        challengeRequiredOnNewDevice: true,
+        deviceOnlyRememberedOnUserPrompt: false,
+      }
+
+    });
+
+    new cognito.CfnUserPoolDomain(this, "Domain", {
+      domain: "imacomputer",
+      userPoolId: pool.ref,
+    });
+
   }
 }
