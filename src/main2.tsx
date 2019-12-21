4160ddc2a1366679d5c36b0896c1deaba1d9cfc7
Anthony Brice <anthonymbrice@gmail.com>
2019-12-21 13:11:57 -0800

    refactor frontend forms

--- src/main2.tsx
+++ src/main2.tsx
@@ -1,5 +1,4 @@
 import * as AWS from "aws-sdk"
-import {Config, CognitoIdentityCredentials} from "aws-sdk";
 import {
   CognitoUserPool,
   CognitoUserAttribute,
@@ -10,9 +9,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import appConfig from "./config";
 
-const config = new Config()
-config.region = appConfig.region;
-config.credentials = new CognitoIdentityCredentials({
+AWS.config.region = appConfig.region;
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
   IdentityPoolId: appConfig.IdentityPoolId
 });
 
@@ -21,14 +19,20 @@ const userPool = new CognitoUserPool({
   ClientId: appConfig.ClientId,
 });
 
-class ChallengeForm extends React.Component<any, any> {
+interface LoginFormState {
+  email: string
+  password: string
+}
 
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
+class EPForm extends React.Component
+  <{submit(e:any, s:LoginFormState, c?:string):void}, LoginFormState> {
+  constructor(props: {submit(e:any, s: LoginFormState):void}) {
+    super(props)
+    this.state = {email: "", password: ""}
+
+    this.handleEmailChange = this.handleEmailChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleEmailChange(e: any) {
@@ -40,12 +44,48 @@ class ChallengeForm extends React.Component<any, any> {
   }
 
   handleSubmit(e: any) {
+    this.props.submit(e, this.state)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text"
+               value={this.state.email}
+               placeholder="Email"
+               onChange={this.handleEmailChange}/>
+        <input type="password"
+               value={this.state.password}
+               placeholder="Password"
+               onChange={this.handlePasswordChange}/>
+        <input type="submit"/>
+      </form>
+    )
+  }
+}
+
+const CognitoSpikeForm = () => {
+  return (
+    <div>
+      <EPForm submit={doLogin}/> <br/>
+      Sign Up
+      <EPForm submit={doSignUp}/> <br/>
+      CJ Challenge
+      <EPForm submit={challenge("269opr68vm5lud18faf7vi8rv6")}/> <br/>
+      3rd Party Challenge
+      <EPForm submit={challenge("37fnmm2dop67vade1i372v1si3")}/>
+    </div>
+  )
+}
+
+function challenge(clientId: string) {
+  return function(e: any, s: LoginFormState) {
     e.preventDefault();
-    const email = this.state.email.trim();
-    const password = this.state.password.trim();
+    const email = s.email.trim();
+    const password = s.password.trim();
     const userPool = new CognitoUserPool({
       UserPoolId: appConfig.UserPoolId,
-      ClientId: this.props.clientId,
+      ClientId: clientId,
     });
     const authenticationData = { Username: email, Password: password }
     , authenticationDetails = new AuthenticationDetails(authenticationData)
@@ -74,162 +114,63 @@ class ChallengeForm extends React.Component<any, any> {
       customChallenge: function(challengeParameters) {
         console.log("challengeParameters", challengeParameters)
         // User authentication depends on challenge response
-        const challengeResponses = confirm(challengeParameters.question)
-          ? 'yes' : 'no'
-        cognitoUser.sendCustomChallengeAnswer(challengeResponses, this);
+        const challengeResponse = confirm(challengeParameters.question) ? 'yes' : 'no'
+        cognitoUser.sendCustomChallengeAnswer(challengeResponse, this);
       },
     });
   }
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit.bind(this)}>
-          <input type="text"
-                 value={this.state.email}
-                 placeholder="Email"
-                 onChange={this.handleEmailChange.bind(this)}/>
-          <input type="password"
-                 value={this.state.password}
-                 placeholder="Password"
-                 onChange={this.handlePasswordChange.bind(this)}/>
-          <input type="submit"/>
-        </form>
-      </div>
-    );
-  }
-
 }
 
-class LoginForm extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
-  }
-
-  handleEmailChange(e: any) {
-    this.setState({email: e.target.value});
-  }
-
-  handlePasswordChange(e: any) {
-    this.setState({password: e.target.value});
-  }
-
-  handleSubmit(e: any) {
-    e.preventDefault();
-    const email = this.state.email.trim();
-    const password = this.state.password.trim();
-    const authenticationData = { Username: email, Password: password }
-    , authenticationDetails = new AuthenticationDetails(authenticationData)
-    , cognitoUser = new CognitoUser(
-      { Username: email
-      , Pool: userPool
-      })
-
-    cognitoUser.setAuthenticationFlowType("CUSTOM_AUTH")
-    cognitoUser.authenticateUser(authenticationDetails, {
-      onSuccess: function(result) {
-        const accessToken = result.getAccessToken().getJwtToken()
-        AWS.config.region = "us-east-1"
-        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-          IdentityPoolId: 'us-east-1:3d2383b5-9c5c-4a6a-be5d-8c095d2ea9a3',
-          Logins: {
-            "cognito-idp.us-east-1.amazonaws.com/us-east-1_e60XYXrIE":
-              result.getIdToken().getJwtToken()
-          }
-        })
-        console.log("result", result)
-        console.log(window.location.search)
-      },
-      onFailure: function(err) {
-        alert(err.message || JSON.stringify(err))
-      },
+function doLogin(e: any, s: LoginFormState) {
+  e.preventDefault();
+  const email = s.email.trim();
+  const password = s.password.trim();
+  const authenticationData = { Username: email, Password: password }
+  , authenticationDetails = new AuthenticationDetails(authenticationData)
+  , cognitoUser = new CognitoUser(
+    { Username: email
+    , Pool: userPool
     })
-  }
 
-  render() {
-    return (
-      <div>
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <input type="text"
-               value={this.state.email}
-               placeholder="Email"
-               onChange={this.handleEmailChange.bind(this)}/>
-        <input type="password"
-               value={this.state.password}
-               placeholder="Password"
-               onChange={this.handlePasswordChange.bind(this)}/>
-        <input type="submit"/>
-      </form> <br/>
-      Sign Up
-        <SignUpForm/>
-        <br/>
-        cj Challenge
-        <ChallengeForm clientId="269opr68vm5lud18faf7vi8rv6"/>
-        <br/>
-        third party Challenge
-        <ChallengeForm clientId="37fnmm2dop67vade1i372v1si3"/>
-      </div>
-  );
-  }
-}
-
-class SignUpForm extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
-  }
-
-  handleEmailChange(e: any) {
-    this.setState({email: e.target.value});
-  }
-
-  handlePasswordChange(e: any) {
-    this.setState({password: e.target.value});
-  }
-
-  handleSubmit(e: any) {
-    e.preventDefault();
-    const email = this.state.email.trim();
-    const password = this.state.password.trim();
-    const attributeList = [
-      new CognitoUserAttribute({
-        Name: 'email',
-        Value: email,
+  cognitoUser.setAuthenticationFlowType("CUSTOM_AUTH")
+  cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: function(result) {
+      const accessToken = result.getAccessToken().getJwtToken()
+      AWS.config.region = "us-east-1"
+      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'us-east-1:3d2383b5-9c5c-4a6a-be5d-8c095d2ea9a3',
+        Logins: {
+          "cognito-idp.us-east-1.amazonaws.com/us-east-1_e60XYXrIE":
+            result.getIdToken().getJwtToken()
+        }
       })
-    ];
-    userPool.signUp(email, password, attributeList, [], (err, result) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log('user name is ' + result?.user.getUsername());
-      console.log('call result: ' + result);
-    });
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <input type="text"
-               value={this.state.email}
-               placeholder="Email"
-               onChange={this.handleEmailChange.bind(this)}/>
-        <input type="password"
-               value={this.state.password}
-               placeholder="Password"
-               onChange={this.handlePasswordChange.bind(this)}/>
-        <input type="submit"/>
-      </form>
-    );
-  }
+      console.log("result", result)
+      console.log(window.location.search)
+    },
+    onFailure: function(err) {
+      alert(err.message || JSON.stringify(err))
+    },
+  })
 }
 
-ReactDOM.render(<LoginForm />, document.getElementById('app'));
+function doSignUp(e: any, s: LoginFormState) {
+  e.preventDefault();
+  const email = s.email.trim();
+  const password = s.password.trim();
+  const attributeList = [
+    new CognitoUserAttribute({
+      Name: 'email',
+      Value: email,
+    })
+  ];
+  userPool.signUp(email, password, attributeList, [], (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('user name is ' + result!.user.getUsername());
+    console.log('call result: ' + result);
+  });
+}
 
+ReactDOM.render(<CognitoSpikeForm />, document.getElementById('app'));
