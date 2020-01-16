e30eebc4dc1c518be13549961bafc9d0e363c7dd
Anthony Brice <anthonymbrice@gmail.com>
2020-01-16 08:59:41 -0800

    Fix build bug for bsd cp

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 
 npx tsc
 npx webpack
-mkdir -p dist/www/ && cp -r www/ dist/
+mkdir -p dist/www/ && /bin/cp -r www dist/
 for x in dist/lam/*-bundle.js; do zip --junk-paths ${x%js}zip $x; done
