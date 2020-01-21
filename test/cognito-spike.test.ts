01fe4e3c130be6a13b19ea2a95bb90972b5d60ff
Anthony Brice <anthonymbrice@gmail.com>
2020-01-21 07:42:19 -0800

    fix tests

--- test/cognito-spike.test.ts
+++ test/cognito-spike.test.ts
@@ -4,6 +4,3 @@ import CognitoSpike = require('../lib/pool');
 
 import { authenticateUser } from "../lib/lam/lib/authCode"
 authenticateUser(
-    { client_id: "4n8eo9c4oceccbfr1k8plm4pdi"
-, redirect_uri: "https://d1gqrvag8c8eck.cloudfront.net"
-    }, { email: "b@b.com", pw: "bbbbbbbb" })
